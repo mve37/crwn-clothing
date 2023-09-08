70991846f50c5bd8fa9dea2114edba42f855acf0
Marielle Vetter <vetter.marielle@guj.de>
2023-09-08 14:51:38 +0200

    Added redux and replace user context and categories context

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,9 +1,23 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Routes, Route } from "react-router-dom";
 
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils.js";
+import { setCategoriesMap } from "../../store/categories/category.action";
 
 const Shop = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getCategoryMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments();
+      dispatch(setCategoriesMap(categoryMap));
+    };
+    getCategoryMap();
+  }, [dispatch]);
+
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
